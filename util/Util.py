42923495cec8d9b53d445803818b6bb321d4f198
Palash Sarkar <palashsarkar0007@gmail.com>
2018-05-27 22:30:54 +0530

    Changed 'getArrayOfGrayLevelsWithFreq()' and '__ins()' methods to utility methods

--- util/Util.py
+++ util/Util.py
@@ -22,12 +22,12 @@ def search(arr,ins_val,l,h):
 
 def quickSort(arr, low, high):
     if low < high:
-        pi = partition(arr, low, high)
+        pi = __partition(arr, low, high)
         quickSort(arr, low, pi - 1)
         quickSort(arr, pi + 1, high)
 
 
-def partition(arr, low, high):
+def __partition(arr, low, high):
     i = (low - 1)
     pivot = (arr[high])[0]
     for j in range(low, high,1):
@@ -40,3 +40,33 @@ def partition(arr, low, high):
     arr[i+1] = copy.deepcopy(arr[high])
     arr[high] = copy.deepcopy(temp2)
     return (i+1)
+
+def __ins(self, arr, ins_val, index, isSearched):
+    if (arr.size == 0):
+        arr = np.insert(arr, index, (ins_val, np.array([ 1 ], np.uint32)), 0)
+    else:
+        flag = 0
+        if (isSearched == 0):
+            fnd_idx = search(arr, ins_val, 0, arr.size-1)
+            if (fnd_idx >= 0):
+                flag = 1
+                ((arr[fnd_idx])[1])[0] = np.uint32(((arr[fnd_idx])[1])[0]) + np.uint32(1)
+        if (flag == 1):
+            pass
+        elif (ins_val > (arr[index - 1])[0]):
+             arr = np.insert(arr, index, (ins_val, np.array([ 1 ], np.uint32)), 0)
+        elif (ins_val < (arr[index - 1])[0]):
+             if (index == 0):
+                arr = np.insert(arr, index, (ins_val, np.array([ 1 ], np.uint32)), 0)
+             else:
+                arr = self.__ins(arr, ins_val, index=index - 1, isSearched=1)
+        else:
+            ((arr[index - 1])[1])[0] = np.uint32(((arr[index - 1])[1])[0]) + np.uint32(1)
+    return arr
+
+def getArrayOfGrayLevelsWithFreq(gray_img):
+    aryoflst = np.empty(0, np.dtype([('glvl', np.uint8), ('freq', np.uint32, (1,))]), 'C')
+    for x in range(0, (gray_img.shape)[0], 1):
+        for y in range(0, (gray_img.shape)[1], 1):
+            aryoflst = __ins(aryoflst, gray_img[x, y], index=aryoflst.size, isSearched=0)
+    return aryoflst
