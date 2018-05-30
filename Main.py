fb105eaecfa3910b4e60329abc97dca69ffc9760
Palash Sarkar <palashsarkar0007@gmail.com>
2018-05-30 20:24:35 +0530

    Added 2dFilter with Prewitt Operators!!!

--- Main.py
+++ Main.py
@@ -12,30 +12,25 @@ def showColImg():
     cv2.imshow('imgcol', obj.getActImg())
     cv2.waitKey(0)
 
-
 def showGrayImg():
     cv2.namedWindow('imggray', cv2.WINDOW_NORMAL)
     cv2.imshow('imggray', obj.getGrayImg())
     cv2.waitKey(0)
-
 
 def showInvertedGrayImg():
     cv2.namedWindow('imggrayinvrt', cv2.WINDOW_NORMAL)
     cv2.imshow('imggrayinvrt', obj.getInvrtGrayImg())
     cv2.waitKey(0)
 
-
 def showBinImg():
     cv2.namedWindow('imgbin', cv2.WINDOW_NORMAL)
     cv2.imshow('imgbin', obj.getBinaryImg())
     cv2.waitKey(0)
 
-
 def showSegmentedColorImg():
     cv2.namedWindow('segimgcol', cv2.WINDOW_NORMAL)
     cv2.imshow('segimgcol', obj.getSegColImg())
     cv2.waitKey(0)
-
 
 def showSegmentedGrayImg():
     cv2.namedWindow('segimggray', cv2.WINDOW_NORMAL)
@@ -80,7 +75,7 @@ def showHaralickFeatures():
     print("Differential-Entropy of seg gray img %f \n" % feobj.getDentropy())
 
 def showTamuraFeatures():
-    #print("Coarseness of seg gray img %f \n" % feobj2.getCoarseness())
+    print("Coarseness of seg gray img %f \n" % feobj2.getCoarseness())
     print("Tamura-Contrast of seg gray img %f \n" % feobj2.getContrast())
     print("Tamura-Kurtosis of seg gray img %f \n" % feobj2.getKurtosis())
 
