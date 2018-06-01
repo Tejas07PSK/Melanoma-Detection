1613173016e2e3d600cddf91be0f8137f7af5b95
Palash Sarkar <palashsarkar0007@gmail.com>
2018-06-01 06:44:11 +0530

    Added 'Regularity' and 'Roughness' calculation modules!!

--- featext/texture/Tamura.py
+++ featext/texture/Tamura.py
@@ -89,12 +89,14 @@ class TamFeat(object):
                 if (np.fabs(delg_img[i, j]) > t):
                     x = int(np.round(np.fabs(d * np.cos(theta_img[i, j]))))
                     y = int(np.round(np.fabs(d * np.sin(theta_img[i, j]))))
-                    if ((x < 0) | (x >= (theta_img.shape)[0]) | (y < 0) | (y >= (theta_img.shape)[1]) | (np.fabs(delg_img[x, y]) <= t)):
+                    if ((x < 0) | (x >= (theta_img.shape)[0]) | (y < 0) | (y >= (theta_img.shape)[1])):
                         continue
                     else:
-                        if ((theta_img[x, y] > (theta_img[i, j] - 0.6)) & (theta_img[x, y] < (theta_img[i, j] + 0.6))):
-                            idx1, idx2 = u.search(dirlevels, theta_img[i, j], 0, dirlevels.size-1), u.search(dirlevels, theta_img[x, y], 0, dirlevels.size-1)
-                            ditfctcm[idx1, idx2] = ditfctcm[idx1, idx2] + 1
+                        if (np.fabs(delg_img[x, y]) <= t):
+                            continue
+                        elif ((theta_img[x, y] > (theta_img[i, j] - 0.6)) & (theta_img[x, y] < (theta_img[i, j] + 0.6))):
+                             idx1, idx2 = u.search(dirlevels, theta_img[i, j], 0, dirlevels.size-1), u.search(dirlevels, theta_img[x, y], 0, dirlevels.size-1)
+                             ditfctcm[idx1, idx2] = ditfctcm[idx1, idx2] + 1
                         else:
                             continue
         varLin = self.__generateVariance(u.getArrayOfGrayLevelsWithFreq(ditfctcm, lvldtype=np.uint32), np.mean(ditfctcm, axis=None, dtype=float))
