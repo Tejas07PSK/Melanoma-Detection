d0a724021592fb76567f355f0067dc0bee846cdd
Palash Sarkar <palashsarkar0007@gmail.com>
2018-06-03 01:04:33 +0530

    'Contour Extraction' work in progress!!!

--- featext/physical/Gabor.py
+++ featext/physical/Gabor.py
@@ -4,7 +4,7 @@ import cv2
 class Gabor:
 
         def __init__(self, img):
-            kernel = cv2.getGaussianKernel(ksize=5, sigma=0, ktype=float)
+            kernel = cv2.getGaussianKernel(ksize=5, sigma=0)
             print(kernel)
             tup = cv2.findContours(cv2.GaussianBlur(img, kernel, sigmaX=0, sigmaY=0), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
             print(tup[0])
