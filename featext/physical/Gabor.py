b72b1353ad0c3e8cdaba8a0238d1b4e8792f854e
Palash Sarkar <palashsarkar0007@gmail.com>
2018-06-03 17:54:52 +0530

    'Contour Extraction' work in progress!!!

--- featext/physical/Gabor.py
+++ featext/physical/Gabor.py
@@ -4,15 +4,18 @@ import cv2
 class Gabor:
 
         def __init__(self, img):
-            tmp = cv2.GaussianBlur(img, (3, 3), sigmaX=0, sigmaY=0)
-            tup = cv2.findContours(tmp, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            tup = cv2.findContours(cv2.GaussianBlur(img, (3, 3), sigmaX=0, sigmaY=0), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            self.__gblurimg = tup[0]
+            self.__contours = tup[1]
+            self.__hierarchy = tup[2]
+            self.__momLstForConts = self.__getMoments(self.__contours)
             tmp = cv2.drawContours(tmp, tup[1], -1, (0, 255, 0), 3, cv2.LINE_AA)
-            cv2.namedWindow('gb', cv2.WINDOW_NORMAL)
-            cv2.imshow('gb', tmp)
-            cv2.waitKey(0)
-            print(tup[0])
-            print(((tup[1])[0])[0])
-            print(cv2.moments((tup[1])[0]))
-            print(((tup[1])[2])[0])
-            print(tup[2])
+            cv2.moments((tup[1])[0])
+
+        def __getMoments(self, contours):
+            moments = []
+            for m in contours:
+                moments.append(cv2.moments(m))
+            print(moments)
+            return moments
 
