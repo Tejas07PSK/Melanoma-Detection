f295d6310d5e144d6a0849430a59f17ff0d083ef
Palash Sarkar <palashsarkar0007@gmail.com>
2018-06-06 13:15:09 +0530

    Testing 'Decision-Tree Classifier' !!!

--- mlmodels/classifiers.py
+++ mlmodels/classifiers.py
@@ -1,15 +1,27 @@
 import numpy as np
 from sklearn.externals import joblib
 from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier as MLP
+from sklearn.tree import DecisionTreeClassifier as DTC
 
 dset, featnames = (np.load('dataset.npz'))['dset'], (np.load('dataset.npz'))['featnames']
 print(list(dset['featureset']))
 print(list(dset['result']))
 X = np.array(list(dset['featureset']))
 Y = np.array(list(dset['result']))
-clf = SVC()
+"""clf = SVC()
 clf.fit(X, Y)
+clf = MLP()
+print(clf)
+clf.fit(X,Y)
+joblib.dump(clf, 'Mel_MLP.pkl')
+print(clf.predict(list(dset['featureset'])))
 joblib.dump(clf, 'Mel_SVM.pkl')
 clf2 = joblib.load('Mel_SVM.pkl')
-print(clf2)
-#print(clf.predict([[1097.01, 0.9987]]))
+print(clf2)"""
+
+clf = DTC()
+clf = clf.fit(X, Y)
+print(clf.predict(X))
+print(clf)
+joblib.dump(clf, 'Mel_DTC.pkl')
