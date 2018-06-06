679b2492bee3dbe48346b6788baf6a7e10ec56c0
Palash Sarkar <palashsarkar0007@gmail.com>
2018-06-07 03:06:22 +0530

    Adding various classifiers!!!

--- mlmodels/classifiers.py
+++ mlmodels/classifiers.py
@@ -56,26 +56,3 @@ class Classifiers(object):
               }
         print(dct)
         return dct
-
-
-dset, featnames = (np.load('dataset.npz'))['dset'], (np.load('dataset.npz'))['featnames']
-print(list(dset['featureset']))
-print(list(dset['result']))
-X = np.array(list(dset['featureset']))
-Y = np.array(list(dset['result']))
-"""clf = SVC()
-clf.fit(X, Y)
-clf = MLP()
-print(clf)
-clf.fit(X,Y)
-joblib.dump(clf, 'Mel_MLP.pkl')
-print(clf.predict(list(dset['featureset'])))
-joblib.dump(clf, 'Mel_SVM.pkl')
-clf2 = joblib.load('Mel_SVM.pkl')
-print(clf2)"""
-
-clf = DTC()
-clf = clf.fit(X, Y)
-print(clf.predict(X))
-print(clf)
-joblib.dump(clf, 'Mel_DTC.pkl')
