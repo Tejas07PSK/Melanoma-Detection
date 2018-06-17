18de1192ae737824af82ce0136700b9943a8aea6
Palash Sarkar <palashsarkar0007@gmail.com>
2018-06-17 06:30:59 +0530

    Graph-Plotting in progress!!!

--- mlmodels/RmFrstDecisionPlot.py
+++ mlmodels/RmFrstDecisionPlot.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 from sklearn import clone
 from sklearn.externals import joblib
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.svm import NuSVC
 from sklearn.svm import LinearSVC
@@ -13,24 +14,41 @@ from sklearn.ensemble import RandomForestClassifier as RFC
 (__no_of_clas, __clas_types, __col_map, __plt_interval, __plt_interval_pp, __rnd_seed) = (3, ('benign', 'malignant', 'negative'), plt.cm.RdYlGn, 0.05, 0.5, 13)
 cloned_classifiers = [joblib.load('mlmodels/Mel_SVM.pkl'), joblib.load('mlmodels/Mel_NuSVM.pkl'), joblib.load('mlmodels/Mel_LinSVM.pkl'), joblib.load('mlmodels/Mel_MLPC.pkl'), joblib.load('mlmodels/Mel_DTC.pkl'), joblib.load('mlmodels/Mel_RFC.pkl')]
 
-def plotForAll(X, Y, ftup):
-    plot_idx = 1
-    titles = ('SVM', 'NuSVM', 'LinSVM', 'MLPC')
-    index = np.arange(X.shape[0])
+def plotForAll(X, Y, ftup, feats):
+    titles = ('SVM', 'NuSVM', 'LinSVM', 'MLPC', 'DTC', 'RFC')
+    index = np.arange(0, X.shape[0], 1)
     for idx_pair in ftup:
-        for mdl in cloned_classifiers[0:5]:
             x = X[:, idx_pair]
             np.random.seed(__rnd_seed)
             np.random.shuffle(index)
             x = x[index]
             y = Y[index]
             x = (x - x.mean(axis=0)) / x.std(axis=0)
-            clf = mdl.fit(x, y)
-            scr = clf.score(X, y)
-            model_title = str(type(mdl)).split(".")[-1][:-2][:-len("Classifier")]
-            plt.subplot(len(ftup), len(cloned_classifiers), plot_idx)
-            if (plot_idx <= len(mdl)):
-                plt.title(model_title)
-            x_min, x_max = x[:, 0].min() - 1, x[:, 0].max() + 1
-            y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-            xx, yy = np.meshgrid(np.arange(x_min, x_max, __plt_interval), np.arange(y_min, y_max, __plt_interval))
+            cor_tup = plt.subplots(ftup.size, len(cloned_classifiers))
+            plt.subplots_adjust(wspace=0.4, hspace=0.4)
+            for i in range(0, ((cor_tup[1]).shape)[0], 1):
+                for mdl, title, j in zip(cloned_classifiers, titles, range(0, ((cor_tup[1]).shape)[1], 1)):
+                    clf = (clone(mdl)).fit(x, y)
+                    scr = clf.score(x, y)
+                    print(scr)
+                    x_min, x_max = x[:, 0].min() - 1, x[:, 0].max() + 1
+                    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+                    xx, yy = np.meshgrid(np.arange(x_min, x_max, ((x_max - x_min) / 100.0)), np.arange(y_min, y_max, ((y_max - y_min) / 100.0)))
+                    if isinstance(mdl, RandomForestClassifier):
+                        alpha_blend = 1.0 / len(mdl.estimators_)
+                        for tree in mdl.estimators_:
+                            ((cor_tup[1])[i, j]).contourf(xx, yy, (tree.predict(np.c_[xx.ravel(), yy.ravel()])).reshape(xx.shape), alpha=alpha_blend, cmap=__col_map)
+                    else:
+                        ((cor_tup[1])[i, j]).contourf(xx, yy, (mdl.predict(np.c_[xx.ravel(), yy.ravel()])).reshape(xx.shape), cmap=__col_map)
+                    xx_coarser, yy_coarser = np.meshgrid(np.arange(x_min, x_max, ((x_max - x_min) / 50.0)), np.arange(y_min, y_max, ((y_max - y_min) / 50.0)))
+                    ((cor_tup[1])[i, j]).scatter(xx_coarser, yy_coarser, s=15, c=mdl.predict(np.c_[xx_coarser.ravel(), yy_coarser.ravel()]).reshape(xx_coarser.shape), cmap=__col_map, edgecolors="none")
+                    ((cor_tup[1])[i, j]).scatter(x[:, 0], x[:, 1], c=y, cmap=__col_map, edgecolor='k', s=20)
+                    ((cor_tup[1])[i, j]).set_xlim(xx.min(), xx.max())
+                    ((cor_tup[1])[i, j]).set_ylim(yy.min(), yy.max())
+                    ((cor_tup[1])[i, j]).set_xlabel(feats[0])
+                    ((cor_tup[1])[i, j]).set_ylabel(feats[1])
+                    ((cor_tup[1])[i, j]).set_xticks(())
+                    ((cor_tup[1])[i, j]).set_yticks(())
+                    ((cor_tup[1])[i, j]).set_title(title)
+    plt.suptitle("Plot of Classifiers on feature subsets of the Melanoma-Dataset")
+    plt.show()
