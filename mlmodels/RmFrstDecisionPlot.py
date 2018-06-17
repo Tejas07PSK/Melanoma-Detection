eeca2c7d8d935d20fadbf281e27e92b3475545cd
Palash Sarkar <palashsarkar0007@gmail.com>
2018-06-17 08:57:07 +0530

    Graph-Plotting in progress!!!

--- mlmodels/RmFrstDecisionPlot.py
+++ mlmodels/RmFrstDecisionPlot.py
@@ -24,11 +24,11 @@ def plotForAll(X, Y, ftup, feats):
             x = x[index]
             y = Y[index]
             x = (x - x.mean(axis=0)) / x.std(axis=0)
-            print(len(ftup))
-            cor_tup = plt.subplots(len(ftup), len(cloned_classifiers))
+            fig, sub_cor = plt.subplots(len(ftup), len(cloned_classifiers))
+            sub_cor = (sub_cor).reshape((len(ftup), len(cloned_classifiers)), order='C')
             plt.subplots_adjust(wspace=0.4, hspace=0.4)
-            for i in range(0, ((cor_tup[1]).shape)[0], 1):
-                for mdl, title, j in zip(cloned_classifiers, titles, range(0, ((cor_tup[1]).shape)[1], 1)):
+            for i in range(0, (sub_cor.shape)[0], 1):
+                for mdl, title, j in zip(cloned_classifiers, titles, range(0, (sub_cor.shape)[1], 1)):
                     clf = (clone(mdl)).fit(x, y)
                     scr = clf.score(x, y)
                     print(scr)
@@ -38,18 +38,18 @@ def plotForAll(X, Y, ftup, feats):
                     if isinstance(mdl, RandomForestClassifier):
                         alpha_blend = 1.0 / len(mdl.estimators_)
                         for tree in mdl.estimators_:
-                            ((cor_tup[1])[i, j]).contourf(xx, yy, (tree.predict(np.c_[xx.ravel(), yy.ravel()])).reshape(xx.shape), alpha=alpha_blend, cmap=__col_map)
+                            (sub_cor[i, j]).contourf(xx, yy, (tree.predict(np.c_[xx.ravel(), yy.ravel()])).reshape(xx.shape), alpha=alpha_blend, cmap=__col_map)
                     else:
-                        ((cor_tup[1])[i, j]).contourf(xx, yy, (mdl.predict(np.c_[xx.ravel(), yy.ravel()])).reshape(xx.shape), cmap=__col_map)
+                        (sub_cor[i, j]).contourf(xx, yy, (mdl.predict(np.c_[xx.ravel(), yy.ravel()])).reshape(xx.shape), cmap=__col_map)
                     xx_coarser, yy_coarser = np.meshgrid(np.arange(x_min, x_max, ((x_max - x_min) / 50.0)), np.arange(y_min, y_max, ((y_max - y_min) / 50.0)))
-                    ((cor_tup[1])[i, j]).scatter(xx_coarser, yy_coarser, s=15, c=mdl.predict(np.c_[xx_coarser.ravel(), yy_coarser.ravel()]).reshape(xx_coarser.shape), cmap=__col_map, edgecolors="none")
-                    ((cor_tup[1])[i, j]).scatter(x[:, 0], x[:, 1], c=y, cmap=__col_map, edgecolor='k', s=20)
-                    ((cor_tup[1])[i, j]).set_xlim(xx.min(), xx.max())
-                    ((cor_tup[1])[i, j]).set_ylim(yy.min(), yy.max())
-                    ((cor_tup[1])[i, j]).set_xlabel(feat[0])
-                    ((cor_tup[1])[i, j]).set_ylabel(feat[1])
-                    ((cor_tup[1])[i, j]).set_xticks(())
-                    ((cor_tup[1])[i, j]).set_yticks(())
-                    ((cor_tup[1])[i, j]).set_title(title)
+                    (sub_cor[i, j]).scatter(xx_coarser, yy_coarser, s=15, c=mdl.predict(np.c_[xx_coarser.ravel(), yy_coarser.ravel()]).reshape(xx_coarser.shape), cmap=__col_map, edgecolors="none")
+                    (sub_cor[i, j]).scatter(x[:, 0], x[:, 1], c=y, cmap=__col_map, edgecolor='k', s=20)
+                    (sub_cor[i, j]).set_xlim(xx.min(), xx.max())
+                    (sub_cor[i, j]).set_ylim(yy.min(), yy.max())
+                    (sub_cor[i, j]).set_xlabel(feat[0])
+                    (sub_cor[i, j]).set_ylabel(feat[1])
+                    (sub_cor[i, j]).set_xticks(())
+                    (sub_cor[i, j]).set_yticks(())
+                    (sub_cor[i, j]).set_title(title)
     plt.suptitle("Plot of Classifiers on feature subsets of the Melanoma-Dataset")
     plt.show()
