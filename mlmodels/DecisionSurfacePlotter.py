3bcfe7f50c241bf42c0f8ac4c6b716488afc9c1e
Palash Sarkar <palashsarkar0007@gmail.com>
2018-06-19 20:58:45 +0530

    Graph-Plotting in progress!!!

--- mlmodels/DecisionSurfacePlotter.py
+++ mlmodels/DecisionSurfacePlotter.py
@@ -11,7 +11,7 @@ from sklearn.neural_network import MLPClassifier as MLPC
 from sklearn.tree import DecisionTreeClassifier as DTC
 from sklearn.ensemble import RandomForestClassifier as RFC
 
-(__no_of_clas, __clas_types, __col_map, __plt_interval, __plt_interval_pp, __rnd_seed) = (3, ('benign', 'malignant', 'negative'), plt.cm.RdYlGn, 0.05, 0.5, 13)
+(__no_of_clas, __clas_types, __col_map, __rnd_seed) = (3, ('benign', 'malignant', 'negative'), plt.cm.RdYlGn, 13)
 cloned_classifiers = [joblib.load('mlmodels/Mel_SVM.pkl'), joblib.load('mlmodels/Mel_NuSVM.pkl'), joblib.load('mlmodels/Mel_LinSVM.pkl'), joblib.load('mlmodels/Mel_MLPC.pkl'), joblib.load('mlmodels/Mel_DTC.pkl'), joblib.load('mlmodels/Mel_RFC.pkl')]
 
 def plotForAll(X, Y, ftup, feats):
