5d8263f93c4e93aa5f4e58615f8c97d4e91e438f
Palash Sarkar <palashsarkar0007@gmail.com>
2018-06-20 01:47:14 +0530

    Display raw prediction results also!!

--- mlmodels/DecisionSurfacePlotter.py
+++ mlmodels/DecisionSurfacePlotter.py
@@ -4,12 +4,6 @@ from matplotlib.colors import ListedColormap
 from sklearn import clone
 from sklearn.externals import joblib
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import SVC
-from sklearn.svm import NuSVC
-from sklearn.svm import LinearSVC
-from sklearn.neural_network import MLPClassifier as MLPC
-from sklearn.tree import DecisionTreeClassifier as DTC
-from sklearn.ensemble import RandomForestClassifier as RFC
 
 (__no_of_clas, __clas_types, __col_map, __rnd_seed) = (3, ('benign', 'malignant', 'negative'), plt.cm.RdYlGn, 13)
 classifiers = [joblib.load('mlmodels/Mel_SVM.pkl'), joblib.load('mlmodels/Mel_NuSVM.pkl'), joblib.load('mlmodels/Mel_LinSVM.pkl'), joblib.load('mlmodels/Mel_MLPC.pkl'), joblib.load('mlmodels/Mel_DTC.pkl'), joblib.load('mlmodels/Mel_RFC.pkl')]
@@ -42,7 +36,7 @@ def plotForAll(X, Y, ftup, feats):
                         obj = plt.subplot(len(ftup), len(classifiers), plot_index)
                         clf = (clone(mdl)).fit(x, y)
                         scr = clf.score(x, y)
-                        print(scr)
+                        print("Feasibility Score For " + scr)
                         x_min, x_max = x[:, 0].min() - 1, x[:, 0].max() + 1
                         y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
                         xx, yy = np.meshgrid(np.arange(x_min, x_max, ((x_max - x_min) / 100.0)), np.arange(y_min, y_max, ((y_max - y_min) / 100.0)))
@@ -55,10 +49,6 @@ def plotForAll(X, Y, ftup, feats):
                         xx_coarser, yy_coarser = np.meshgrid(np.arange(x_min, x_max, ((x_max - x_min) / 50.0)), np.arange(y_min, y_max, ((y_max - y_min) / 50.0)))
                         (obj).scatter(xx_coarser, yy_coarser, s=15, c=clf.predict(np.c_[xx_coarser.ravel(), yy_coarser.ravel()]).reshape(xx_coarser.shape), cmap=__col_map, edgecolors="none")
                         (obj).scatter(x[:, 0], x[:, 1], c=y, marker='p', cmap=__col_map, edgecolor='k', s=20)
-                        print((xx.min(), xx.max()))
-                        print((yy.min(), yy.max()))
-                        print(xx.max() - xx.min())
-                        print(yy.max() - yy.min())
                         (obj).set_xlim(xx.min(), xx.max())
                         (obj).set_ylim(yy.min(), yy.max())
                         (obj).set_xlabel(feat[0])
