78bb6279e6deaef8f05418a97fef7558b22ef8ff
Palash Sarkar <palashsarkar0007@gmail.com>
2018-06-23 00:49:21 +0530

    Multi-threaded optimization while calculating 'Tamura-Coarseness'!!

--- featext/texture/Tamura.py
+++ featext/texture/Tamura.py
@@ -26,7 +26,7 @@ class TamFeat(object):
             for y in range(0, (src_img.shape)[1], 1):
                 emax = np.empty(0, np.dtype([('E', float), ('K', int)]), 'C')
                 for k in range(1, 7, 1):
-                    tds = [Thread(target=self.__nebAvg, name='Cor0', args=(self, x + np.float_power(2, k-1), y, k, src_img, lock, Event(), 0)), Thread(target=self.__nebAvg, name='Cor1', args=(self, x - np.float_power(2, k-1), y, k, src_img, lock, Event(), 1)), Thread(target=self.__nebAvg, name='Cor2', args=(self, x, y + np.float_power(2, k-1), k, src_img, lock, Event(), 2)), Thread(target=self.__nebAvg, name='Cor3', args=(self, x, y - np.float_power(2, k-1), k, src_img, lock, Event(), 3))]
+                    tds = [Thread(target=self.__nebAvg, name='Cor0', args=(x + np.float_power(2, k-1), y, k, src_img, lock, Event(), 0)), Thread(target=self.__nebAvg, name='Cor1', args=(x - np.float_power(2, k-1), y, k, src_img, lock, Event(), 1)), Thread(target=self.__nebAvg, name='Cor2', args=(x, y + np.float_power(2, k-1), k, src_img, lock, Event(), 2)), Thread(target=self.__nebAvg, name='Cor3', args=(x, y - np.float_power(2, k-1), k, src_img, lock, Event(), 3))]
                     self.__tds_opt(tds)
                     self.__tds_opt(tds, 'j')
                     nbavgs = self.__getFromQueue()
