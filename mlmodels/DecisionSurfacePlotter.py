9bef6b3e463ebcd2f9cb4ca4497a4b45351cb96e
Palash Sarkar <palashsarkar0007@gmail.com>
2018-06-24 10:40:55 +0530

    Trying to add legend again!!

--- mlmodels/DecisionSurfacePlotter.py
+++ mlmodels/DecisionSurfacePlotter.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as Pchs
 from sklearn import clone
 from sklearn.externals import joblib
 from sklearn.ensemble import RandomForestClassifier
